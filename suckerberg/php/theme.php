5a57a9ae9bd8f8e18a748f438cdd96a33293179c
DeadSuperHero <sean@deadsuperhero.com>
2014-10-08 01:29:55 -0500

    Suckerberg take 2

---
+++ suckerberg/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Suckerberg
+ *   * Description: A Facebook-like theme based on redbasic
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function mytheme_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
