5eb778cd33bad4d21208160274827108118f4936
DeadSuperHero <sean@deadsuperhero.com>
2014-10-15 18:38:54 -0500

    new theme, also try to make oembed videos suck way less

---
+++ stumble/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Stumble
+ *   * Description: A Tumblr-like theme based on redbasic
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function stumble_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
