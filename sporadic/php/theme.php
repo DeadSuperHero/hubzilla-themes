b928091f0d2f9c116589ccfa47b4610fd75274c4
DeadSuperHero <sean@deadsuperhero.com>
2014-10-21 17:43:36 -0500

    push sporadic theme

---
+++ sporadic/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Sporadic
+ *   * Description: A Diaspora-like theme based on redbasic
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function sporadic_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
