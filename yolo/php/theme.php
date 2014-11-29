db5b9e1c9d4fe35b6c7ca580048f0bc7553e9a9e
DeadSuperHero <sean@deadsuperhero.com>
2014-11-29 01:54:17 -0600

    basic theme entries for yolo. CSS inbound.

---
+++ yolo/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: yolo
+ *   * Description: An Ello-like theme based on redbasic. Because fuck Ello.
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function stumble_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
