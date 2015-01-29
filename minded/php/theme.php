0c490ec56e1beb69432e80f3feaba475015f785f
DeadSuperHero <sean@deadsuperhero.com>
2015-01-28 22:25:07 -0600

    Begin work on Minded theme

---
+++ minded/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Minded
+ *   * Description: A port of the Minds.com style to Red
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function minded_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
