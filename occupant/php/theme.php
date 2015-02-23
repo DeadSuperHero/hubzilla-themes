f38ea2f9c2c9f170e7871831e3f0a80d921233b5
DeadSuperHero <sean@deadsuperhero.com>
2015-02-23 03:22:50 -0600

    Occupy theme

---
+++ occupant/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Sunbeam
+ *   * Description: A fresh theme based on redbasic
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function occupant_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
