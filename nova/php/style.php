0bf41e1c254b855c9cb5e80bdcd217d9912ee8c3
DeadSuperHero <sean@deadsuperhero.com>
2015-03-05 23:54:19 -0600

    nova theme

---
+++ nova/php/style.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('view/theme/redbasic/php/style.php');
+
+echo @file_get_contents('view/theme/nova/css/style.css');
---
+++ nova/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Nova
+ *   * Description: A new style for based on Peoria.in
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function nova_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
