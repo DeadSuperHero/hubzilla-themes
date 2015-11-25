0cee7413c650a76caebffa1bbca11a3deb07756d
Toilet <sean@deadsuperhero.com>
2015-11-24 19:48:58 -0600

    start vk theme

---
+++ verykool/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: VeryKool
+ *   * Description: An interface clone of the VK social network. For, uh, you know, Russians.
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Red [*]
+ *
+ */
+
+function hivenet_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
