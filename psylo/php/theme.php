7c18f9a9fab759bb18ab16d74f9d0a6b2190b656
Sean Tilley <sean@deadsuperhero.com>
2016-03-31 01:03:22 -0500

    initial Psylo commit. Very hacky, will eventually consider decoupling it from RedBasic. Feel pretty apprehensive about what I am supposed to do. Here be dragons

---
+++ psylo/php/theme.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Psylo
+ *   * Description: Experimental Hubzilla UX. Extremely broken.
+ *   * Version: 0.01
+ *   * Author: Sean Tilley
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function psylo_init(&$a) {
+
+    $a->theme_info['extends'] = 'redbasic';
+
+
+}
