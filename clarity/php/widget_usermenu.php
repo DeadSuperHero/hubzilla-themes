7491853108e8b835ac2cfdd50416b4fc0b6905cd
deadsuperhero <sean@deadsuperhero.com>
2017-12-08 15:59:42 -0800

    make widget method more macgirvinesque

--- clarity/php/widget_usermenu.php
+++ clarity/php/widget_usermenu.php
@@ -1,7 +1,9 @@
 <?php
 
 function widget_usermenu($args) {
-  $tpl = get_markup_template('user_menu.tpl'), array(
-    '$banner' => t('User Menu')
+  $o = replace_macros(get_markup_template('user_menu.tpl'), array(
+    '$banner' => t('User Menu'),
   ));
+  return $o;
+  
   }
