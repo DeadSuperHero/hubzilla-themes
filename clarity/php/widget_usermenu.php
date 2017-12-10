5adc137cf934f1e3bc61c26fbcc73826c3c23db9
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 12:19:48 -0800

    fix the widget part

--- clarity/php/widget_usermenu.php
+++ clarity/php/widget_usermenu.php
@@ -3,7 +3,9 @@
 function widget_usermenu($args) {
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
     '$banner' => t('User Menu'),
+    '$app' => $papp,
+    '$icon' => $icon,
   ));
   return $o;
-  
+
   }
