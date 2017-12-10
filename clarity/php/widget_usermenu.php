363b553c82eac985fc90517fbc6f3b1dcddecc10
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 12:22:53 -0800

    super gross hack, let us see what happens

--- clarity/php/widget_usermenu.php
+++ clarity/php/widget_usermenu.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once('include/plugin.php');
+require_once('include/channel.php');
+require_once('Zotlabs/Lib/Apps.php');
+
 function widget_usermenu($args) {
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
     '$banner' => t('User Menu'),
