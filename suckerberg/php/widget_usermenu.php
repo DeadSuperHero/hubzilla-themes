6d2e4ace1cfa0f10121fd5ce559c969aac369211
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 22:26:38 -0800

    fix block

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -5,9 +5,9 @@ require_once('include/channel.php');
 
 function widget_usermenu($args) {
 
-  if(local_channel())
-      $channel = channelx_by_n(\App::$profile_uid)
-      return;
+  if(local_channel()) {
+      $channel = channelx_by_n(\App::$profile_uid);
+  }
 
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
     '$banner' => $channel.name
