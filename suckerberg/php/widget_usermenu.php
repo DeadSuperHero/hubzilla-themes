728feaef903669dbaf1a8b541ba19c0fcec4fe73
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 18:58:34 -0800

    new attempt3

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -6,7 +6,6 @@ function widget_usermenu($args) {
 
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
     '$photo' => $photo
-    '$user' = $channel['channel_name'];
   ));
   return $o;
 
