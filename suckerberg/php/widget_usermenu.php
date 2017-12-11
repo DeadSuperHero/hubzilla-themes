f17e13e84c82d9ad7321c16d733b0128c441e3c6
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 19:06:19 -0800

    revert

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -2,14 +2,12 @@
 
 require_once('include/network.php');
 require_once('include/channel.php');
-$o = '';
 
 function widget_usermenu($args) {
 
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
     '$photo' => $photo
   ));
-  return vcard_from_xchan('', \App::get_observer());
   return $o;
 
   }
