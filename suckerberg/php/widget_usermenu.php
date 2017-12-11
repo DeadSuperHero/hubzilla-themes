1b6cac3e3143f5c8a3ef9b87ab6b151cfd2270eb
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 19:17:26 -0800

    maybe it will break again

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -6,7 +6,7 @@ require_once('include/channel.php');
 function widget_usermenu($args) {
 
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
-    '$banner' => t("$channel.channel_name")
+    '$banner' => $channel.channel_name
     '$photo' => $channel.photo_url
   ));
   return $o;
