80a11fca61a8974abd692cbea4405b0001224e22
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 19:04:31 -0800

    fudge

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -1,12 +1,14 @@
 <?php
 
 require_once('include/network.php');
+require_once('include/channel.php');
+$o = '';
 
 function widget_usermenu($args) {
 
-  $o = replace_macros(get_markup_template('user_menu.tpl'), array(
-    '$photo' => $photo
-  ));
+  $o = replace_macros(get_markup_template('user_menu.tpl')
+  );
+  return vcard_from_xchan('', \App::get_observer());
   return $o;
 
   }
