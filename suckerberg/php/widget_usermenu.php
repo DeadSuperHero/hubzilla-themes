d678f7d4e1bab9d7c6981627e4e9ed0f1b1a6009
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 22:25:01 -0800

    move block

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -3,11 +3,12 @@
 require_once('include/network.php');
 require_once('include/channel.php');
 
-if(local_channel()) {
-    $channel = channelx_by_n(\App::$profile_uid);
-}
-
 function widget_usermenu($args) {
+
+  if(local_channel()) {
+      $channel = channelx_by_n(\App::$profile_uid)
+      return;
+
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
     '$banner' => $channel.name
   ));
