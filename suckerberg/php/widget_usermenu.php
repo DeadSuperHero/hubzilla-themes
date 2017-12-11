d1671464aa08c339458c5d19fe8a54a04a391a78
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 22:07:16 -0800

    tweak channel call

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -3,11 +3,13 @@
 require_once('include/network.php');
 require_once('include/channel.php');
 
-function widget_usermenu($args) {
+if(local_channel()) {
+    $channel = channelx_by_n(\App::$profile_uid);
+}
 
+function widget_usermenu($args) {
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
     '$banner' => $channel.channel_name
-    '$photo' => $channel.photo_url
   ));
   return $o;
 
