1d7a92adc042834c3fc9f4948c11491627b2452f
deadsuperhero <sean@deadsuperhero.com>
2017-12-10 18:57:29 -0800

    new attempt

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -5,7 +5,8 @@ require_once('include/network.php');
 function widget_usermenu($args) {
 
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
-    '$photo' => $profile.photo
+    '$photo' => $photo
+    '$user' = $channel_url
   ));
   return $o;
 
