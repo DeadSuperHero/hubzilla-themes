8c4f5a81a3dbd01b615339d3f7710ea39d4c1b32
deadsuperhero <sean@deadsuperhero.com>
2017-12-13 13:17:52 -0800

    try substituting with poi

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -6,7 +6,7 @@ require_once('include/channel.php');
 function widget_usermenu($args) {
 
   if(local_channel()) {
-      $channel = channelx_by_n(\App::$profile_uid);
+      $channel = channelx_by_n(\App::$poi);
   }
 
   $o = replace_macros(get_markup_template('user_menu.tpl'), array(
