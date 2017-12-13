8e1943c83ffbfc4848da4facf83e5c15fc9fbffa
deadsuperhero <sean@deadsuperhero.com>
2017-12-13 14:24:26 -0800

    try this instead?

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -5,10 +5,6 @@ require_once('include/channel.php');
 
 function widget_usermenu($args) {
 
-    if(! local_channel()) {
-      $local_channel = false;
-    }
-
     $channel = channelx_by_n(\App::$profile_uid);
 
     $o = replace_macros(get_markup_template('user_menu.tpl'), array(
