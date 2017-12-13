654335a75aae5ffa3859a41e8c3785a3e48fa4f7
deadsuperhero <sean@deadsuperhero.com>
2017-12-13 14:12:34 -0800

    maybe I do not actually need this loop

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -5,9 +5,7 @@ require_once('include/channel.php');
 
 function widget_usermenu($args) {
 
-    if(local_channel()) {
-        $channel = channelx_by_n(\App::$profile['profile_uid']);
-      }
+    $channel = channelx_by_n(\App::$profile_uid);
 
     $o = replace_macros(get_markup_template('user_menu.tpl'), array(
       '$banner' => $channel['channel_name'],
