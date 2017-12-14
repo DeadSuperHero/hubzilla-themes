8202185870076f5d3d0a9d23615f5d9fa6fd1bf3
deadsuperhero <sean@deadsuperhero.com>
2017-12-13 17:16:17 -0800

    experiment with logic loop, provide fallback if no local channel is available

--- suckerberg/php/widget_usermenu.php
+++ suckerberg/php/widget_usermenu.php
@@ -2,6 +2,10 @@
 
 function widget_usermenu($args) {
 
+    if(! local_channel()) {
+      $channel = \App::get_channel();
+    }
+
     $channel = channelx_by_n(\App::$profile_uid);
 
     $o = replace_macros(get_markup_template('user_menu.tpl'), array(
