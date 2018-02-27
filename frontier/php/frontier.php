3540f58bb2118076dac629d2efe2aa15c3c7e768
deadsuperhero <sean@deadsuperhero.com>
2018-02-27 14:06:24 -0800

    create frontier theme, used only for landing pages

---
+++ frontier/php/frontier.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *   * Name: Frontier
+ *   * Description: A theme intended for landing pages
+ *   * Version: 1.0
+ *   * Author: Sean Tilley
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function frontier_init(&$a) {
+
+    App::$theme_info['extends'] = 'redbasic';
+
+
+}
