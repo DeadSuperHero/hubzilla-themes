5d514f9150e733f64e3dffa349fa5cd0015fc247
deadsuperhero <sean@deadsuperhero.com>
2018-02-27 14:10:25 -0800

    fix

--- frontier/php/frontier.php
+++ frontier/php/frontier.php
@@ -4,7 +4,7 @@
  *   * Name: Frontier
  *   * Description: A theme intended for landing pages
  *   * Version: 0.1
- *   * MinVersion: 2.3.1
+ *   * MinVersion: 0.1
  *   * MaxVersion: 3.5
  *   * Author: Sean Tilley
  *   * Compat: Hubzilla [*]
