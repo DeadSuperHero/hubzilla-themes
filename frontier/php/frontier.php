11ef8973e83bbdea28042da68864d5d9e6c9526d
deadsuperhero <sean@deadsuperhero.com>
2018-02-27 14:11:48 -0800

    adjustments

--- frontier/php/frontier.php
+++ frontier/php/frontier.php
@@ -4,14 +4,12 @@
  *   * Name: Frontier
  *   * Description: A theme intended for landing pages
  *   * Version: 0.1
- *   * MinVersion: 0.1
- *   * MaxVersion: 3.5
  *   * Author: Sean Tilley
  *   * Compat: Hubzilla [*]
  *
  */
 
-function frontier_init(&$a) {
+function frontier_init(&$App) {
 
     App::$theme_info['extends'] = 'redbasic';
 
