f1435151513ea9a95c19bbe62d8c109d958387ed
deadsuperhero <sean@deadsuperhero.com>
2018-03-22 14:24:45 -0700

    introduce new theme, Reader

---
+++ reader/php/theme.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *   * Name: Reader
+ *   * Description: A simple theme for article hubs
+ *   * Version: 2.1
+ *   * MinVersion: 2.3.1
+ *   * MaxVersion: 3.5
+ *   * Author: Sean Tilley
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function reader_init(&$App) {
+
+    App::$theme_info['extends'] = 'redbasic';
+
+
+}
