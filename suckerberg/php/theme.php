f1a0e0b4b4e5fb6f5b11c23fbaa9248c951f22ca
deadsuperhero <sean@deadsuperhero.com>
2018-04-25 11:41:26 -0700

    bump max version for Suckerberg; just a test

--- suckerberg/php/theme.php
+++ suckerberg/php/theme.php
@@ -5,7 +5,7 @@
  *   * Description: A Facebook-like theme based on redbasic
  *   * Version: 2.1
  *   * MinVersion: 2.3.1
- *   * MaxVersion: 3.5
+ *   * MaxVersion: 4.0
  *   * Author: Sean Tilley
  *   * Compat: Hubzilla [*]
  *
